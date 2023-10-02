af0ad86b24963d7a0f9e54a7144a5502aafcce48
PrGhostDev <139947007+PrGhostDev@users.noreply.github.com>
2023-10-02 16:03:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function updateClock() {
+    const timerElement = document.getElementById("timer");
+    const now = new Date();
+
+    const date = now.toLocaleDateString();
+    const time = now.toLocaleTimeString();
+
+    timerElement.textContent = `${date} ${time}`;
+}
+
+setInterval(updateClock, 1000);
+
+updateClock();
